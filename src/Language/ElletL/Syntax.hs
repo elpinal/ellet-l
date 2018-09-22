4603b541583b9951dbe022a8cbb491f5a582a977
elpinal <elpinal@users.noreply.github.com>
2018-09-22 09:12:35 +0900

    Add types to represent types

--- src/Language/ElletL/Syntax.hs
+++ src/Language/ElletL/Syntax.hs
@@ -6,7 +6,15 @@ module Language.ElletL.Syntax
   , Inst(..)
   , Terminator(..)
   , Block(..)
+
+  -- * Types
+  , Context(..)
+  , Type(..)
+  , LType(..)
+  , MType(..)
   ) where
+
+import qualified Data.Map.Lazy as Map
 
 newtype Reg = Reg Int
   deriving (Eq, Ord, Show)
@@ -37,3 +45,22 @@ data Terminator
   | Halt
 
 data Block = Block [Inst] Terminator
+
+
+newtype Context = Context { getContext :: Map.Map Reg LType }
+
+data Type
+  = Forall Type
+  | TInt
+  | Word
+  | Code Context
+
+data LType
+  = TVar Int
+  | Type Type
+  | Nullable MType
+  | Ref MType
+  | Exist LType
+  | Rec LType
+
+data MType = MType LType LType
